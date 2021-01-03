83bcd1cc6161072c466ab6298d21b5b48381a563
kevin <hyunbin7303@gmail.com>
2021-01-02 22:30:25 -0500

    Update Solution for GRPC.

---
+++ grpcService/grpcClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using grpcService;
+using System;
+using System.Threading.Tasks;
+namespace grpcClient
+{
+
+    // both side should know about proto files. 
+
+    class Program
+    {
+        private static GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+        static async Task Main(string[] args)
+        {
+
+            await testing_ServerStreamingCall();
+
+        }
+
+        static async Task testing_basic()
+        {
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = await client.SayHelloAsync(new HelloRequest
+            {
+                Name = ".Net Conf"
+            });
+            Console.WriteLine("From server : " + response.Message);
+        }
+        static async Task testing_ServerStreamingCall()
+        {
+            var client = new Greeter.GreeterClient(channel);
+            var call = client.SayHelloStream(new HelloRequest
+            {
+                Name = "Kevin Park Streaming "
+            });
+            await foreach (var item in call.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine("From Server : "+ item.Message);
+            }
+        }
+
+
+        //gRPC workflow take place:
+        //Protobufs --> protoc --> generated code /stubs --> client/server
+        // You create your proto files, defining nol
+
+    }
+}
---
+++ grpcService/grpcService/Services/TodoService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace grpcService.Services
+{
+    public class TodoService/* Todos.*/
+    {
+    }
+}
